ac21c6de5f34b64b54d51203d25b1d87d0c4486a
AlexDNZ <paleozene@gmail.com>
2020-11-14 15:36:25 +0100

    normized ft_memset.c

--- ft_memset.c
+++ ft_memset.c
@@ -6,7 +6,7 @@
 /*   By: adenhez <adenhez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 22:43:42 by adenhez           #+#    #+#             */
-/*   Updated: 2020/11/12 22:49:24 by adenhez          ###   ########.fr       */
+/*   Updated: 2020/11/14 15:20:53 by adenhez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,6 @@ void	*ft_memset(void *s, int c, size_t n)
 {
 	unsigned char	*str;
 
-//	if (s == NULL)
-//		return (NULL);
 	str = (unsigned char *)s;
 	while (n--)
 	{
