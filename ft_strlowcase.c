0fa7e7d6422b162761cf19e76c39e03069c12fb5
AlexDNZ <paleozene@gmail.com>
2020-11-14 14:51:53 +0100

    remove malloc operation

--- ft_strlowcase.c
+++ ft_strlowcase.c
@@ -6,7 +6,7 @@
 /*   By: adenhez <adenhez@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/10 23:06:16 by adenhez           #+#    #+#             */
-/*   Updated: 2020/11/11 00:02:12 by adenhez          ###   ########.fr       */
+/*   Updated: 2020/11/14 14:08:39 by adenhez          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,13 @@
 char	*ft_strlowcase(char *s)
 {
 	int		i;
-	char	*str;
 
-	i = 0;
-	while (s[i])
-		i++;
-	str = malloc(sizeof(char) * (i + 1));
-	if (str == NULL)
-		return (NULL);
 	i = 0;
 	while (s[i])
 	{
 		if (s[i] >= 65 && s[i] <= 90)
-			str[i] = s[i] + 32;
-		else
-			str[i] = s[i];
+			s[i] += 32;
 		i++;
 	}
-	str[i] = 0;
-	return (str);
+	return (s);
 }
